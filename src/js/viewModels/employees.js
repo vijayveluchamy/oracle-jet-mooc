7a00ef526a4490564974169006aa7c382fb353ce
vvelucha <vijay.veluchamy@oracle.com>
2018-05-16 22:05:00 +0530

    initial commit

---
+++ src/js/viewModels/employees.js
@@ -0,0 +1,11 @@
+define(['ojs/ojcore', 'knockout', 'jquery'],
+ function(oj, ko, $) {
+  
+    function EmployeeViewModel() {
+      var self = this;
+     
+    }
+
+    return new EmployeeViewModel();
+  }
+);
